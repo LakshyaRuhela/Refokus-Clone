6d6ed218b8124c0516da65fd31c4090014acf508
Lakshya Ruhela <ruhelalakshya3@gmail.com>
2026-01-24 00:12:23 +0530

    Work component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
- import React from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
- function App(){
+import Work from "./components/Work";
+
+function App() {
   return (
     <div className="w-full h-screen bg-zinc-900 text-white font-['santoshi'] ">
-      <Navbar/>
+      <Navbar />
+      <Work/>
     </div>
-  )
- }
- export default App;
+  );
+}
+export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { IoMdReturnRight } from "react-icons/io";
 function Button() {
   return (
-    <div className="min-w-40 px-3 py-2 bg-zinc-100 rounded-full text-black flex items-center justify-between">
+    <div className="min-w-40 px-3 py-2 bg-zinc-100 rounded-full text-black flex items-center justify-between cursor-pointer">
       <span className=" text-sm font-medium">Get Started</span>
       <IoMdReturnRight />
     </div>
